db79cbd8991ab57bece119e692bcfee8077252d6
professionalcarsuk <professionalcarsuk@gmail.com>
2026-01-19 16:06:44 +0000

    finance v7

--- src/pages/Finance/Finance.jsx
+++ src/pages/Finance/Finance.jsx
@@ -41,7 +41,7 @@ const Finance = () => {
  
       <br />
       <br />
-      <ul className="space-y-2">
+      <ul>
             <li><p><strong>Why Finance Through a Dealership?</strong> <iframe src="https://player.vimeo.com/video/102311026" width="500" height="281" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" style="max-width:100%;"></iframe></p></li>
 
 <li><p><strong>What is Hire Purchase?</strong> <iframe src="https://player.vimeo.com/video/106279545" width="500" height="281" frameborder="0" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" style="max-width:100%;"></iframe></p></li>
